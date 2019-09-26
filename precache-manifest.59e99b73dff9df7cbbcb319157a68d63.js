083753e24532bf4939ac4d3bbf6773b8cb1a9c7a
kamsi09 <kamsioduche85@gmail.com>
2019-09-26 14:44:33 -0500

    Updates

---
+++ precache-manifest.59e99b73dff9df7cbbcb319157a68d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "61c1dc02d057923c6877",
+    "url": "/myapp/static/js/main.50e0ea48.chunk.js"
+  },
+  {
+    "revision": "7be94a36a60e20a2692b",
+    "url": "/myapp/static/js/2.3dc92966.chunk.js"
+  },
+  {
+    "revision": "61c1dc02d057923c6877",
+    "url": "/myapp/static/css/main.151cfa69.chunk.css"
+  },
+  {
+    "revision": "7be94a36a60e20a2692b",
+    "url": "/myapp/static/css/2.f24dc9aa.chunk.css"
+  },
+  {
+    "revision": "9f0c50133e9a0375c82f139815fbb22c",
+    "url": "/myapp/index.html"
+  }
+];
